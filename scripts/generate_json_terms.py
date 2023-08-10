2ff2dc588ab7cfe0e94849d3c9ddb18f7751ff60
JonasVHG <4658984+JonasVHG@users.noreply.github.com>
2023-08-10 10:42:41 +0200

    Remove legacy themes

--- scripts/generate_json_terms.py
+++ scripts/generate_json_terms.py
@@ -20,6 +20,11 @@ event_place_facilities = [
     '3.25.0.0.0', # Contactpunt voor personen met handicap
     '3.26.0.0.0', # Parkeerplaats
 ]
+other_legacy_terms = [
+    '0.100.0.0.0', # Kijken en luisteren
+    '0.100.1.0.0', # Doen
+    '0.100.2.0.0', # Bezoeken
+]
 theme_list = []
 terms_list = []
 place_types = [
@@ -340,6 +345,9 @@ for categorisation in root.findall('{http://www.cultuurdatabank.com/XMLSchema/Cd
         enabled = term.get('enabled')
 
         if enabled == 'false':
+            continue
+
+        if id in other_legacy_terms:
             continue
 
         if domain == 'eventtype' or domain == 'theme' or domain == 'facility':
